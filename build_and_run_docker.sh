3dd5f91e1d182c5b59c33d112d42c3598b556b8f
Nikita Kazeev <kazeevn@yandex-team.ru>
2016-06-19 23:23:32 +0200

    Added seaborn and libgfortran

--- build_and_run_docker.sh
+++ build_and_run_docker.sh
@@ -5,5 +5,5 @@
 set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-docker build -t mlhep-2016 $DIR
-docker run -it -p 127.0.0.1:8888:8888 -v "$DIR":/root/analysis mlhep-2016 $1
+docker build -t kazeevn/mlhep-2016:stable_v2 $DIR
+docker run -it -p 127.0.0.1:8888:8888 -v "$DIR":/root/analysis kazeevn/mlhep-2016:stable_v2 $1
